a32eee6c2e3052d3233a6b8ded4be0aa8d3f7911
JIA WEI <130660308+Alexis99997777@users.noreply.github.com>
2025-10-07 23:56:17 -0700

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1,11 +1,16 @@
 import ImageBanner from "@/components/ImageBanner";
 import Products from "@/components/Products"
 
-export async function getProducts(){
-  const baseURL = process.env.NEXT_PUBLIC_BASE_URL
-  const response = await fetch( baseURL + '/api/products' )
-  const products = await response.json()
-  return products
+export async function getProducts()export async function getProducts(){
+  try {
+        const baseURL = process.env.NEXT_PUBLIC_BASE_URL
+        const response = await fetch(baseURL + '/api/products')
+        const products = await response.json()
+        return products
+    } catch (err) {
+        console.log(err.message)
+        return []
+    }
 }
 
 
