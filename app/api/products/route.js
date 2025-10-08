7551ed5c3fba2a790848a5dc43e23d4e7097a498
JIA WEI <130660308+Alexis99997777@users.noreply.github.com>
2025-10-08 10:27:28 -0700

    Update route.js

--- app/api/products/route.js
+++ app/api/products/route.js
@@ -2,8 +2,8 @@ import Stripe  from "stripe";
 import '../../../envConfig.js'
 
 const API_KEY = process.env.NEXT_PUBLIC_STRIPE_SECRET_KEY
-const stripe = new Stripe(API_KEY,{
-    apiVersion:"2023-10-16"
+const stripe = new Stripe(API_KEY, {
+    apiVersion: '2023-10-16'
 })
 
 export async function GET(){
